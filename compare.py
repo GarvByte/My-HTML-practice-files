48c3f11ba2b99f7c4d32029826c20c817eafb76a
GarvByte <garv240121v@gmail.com>
2025-06-29 19:59:10 +0530

    initial commit

---
+++ compare.py
@@ -0,0 +1,12 @@
+from tkinter import N
+
+
+x = [1,2,3]
+y =[[1,2,3]]
+
+for i in range(len(y)):
+    for j in range(len(y[i])):
+        print(y[0])
+        if x == y[0]:
+            print("YES")
+
---
+++ reverse.py
@@ -0,0 +1,8 @@
+x = "hello"
+
+# for i in range(len(x)):
+    # print(x[i])
+y = len(x)
+for j in range(-1:-5):
+    
+    print(x)
